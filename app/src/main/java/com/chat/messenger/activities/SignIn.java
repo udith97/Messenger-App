2d3ecd1b417882fb90265ad437ed653b7ff57cc3
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2021-11-29 17:50:52 +0530

    sign in

--- app/src/main/java/com/chat/messenger/activities/SignIn.java
+++ app/src/main/java/com/chat/messenger/activities/SignIn.java
@@ -4,21 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
-import com.chat.messenger.R;
 import com.chat.messenger.databinding.ActivitySignInBinding;
+import com.chat.messenger.utilities.Constants;
+import com.chat.messenger.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
 
 public class SignIn extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -27,21 +31,61 @@ public class SignIn extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(view ->
                 startActivity(new Intent(getApplicationContext(), SignUp.class)));
-        binding.btnSignIn.setOnClickListener(view -> addDataToFirestore());
+        binding.btnSignIn.setOnClickListener(view -> {
+            if (isValidSignInDetails()){
+                signIn();
+            }
+        });
     }
 
-    private void addDataToFirestore() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("first_name", "Udith");
-        data.put("last_name", "Jayasinghe");
-        db.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(), "DataInserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception ->{
-                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+    private void signIn() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                   if (task.isSuccessful() && task.getResult() != null
+                   && task.getResult().getDocuments().size() > 0) {
+                       DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                       preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                       preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                       preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                       preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }else {
+                       loading(false);
+                       showToast("Unable to sign in");
+                   }
                 });
+    }
+
+    private void loading(Boolean isLoading){
+        if (isLoading){
+            binding.btnSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.btnSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails() {
+        if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        }else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter password");
+            return false;
+        }else{
+            return true;
+        }
     }
 }
